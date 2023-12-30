bccbab89fe6c5defe0a92712059b46418b529b57
annisaisme <annisasalsabilla112@gmail.com>
2023-12-30 22:55:37 +0700

    profile annisa

---
+++ src/screens/profile.js
@@ -0,0 +1,78 @@
+import {TouchableOpacity} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeBaseProvider, Stack } from 'native-base';
+import { View, Text, Image, Heading, Box, ScrollView, SafeAreaView, Avatar } from 'native-base';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const MyProfile = ({ navigation }) => {
+  return (
+    <NativeBaseProvider>
+      <SafeAreaView flex={1}>
+      <ScrollView>
+        <Heading textAlign="center" mt="10">
+          My Profile
+        </Heading>
+        <Avatar bg="purple.600" alignSelf="center" size="2xl" mt="5" source={{
+        uri: "https://images.unsplash.com/photo-1541823709867-1b206113eafd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }}>
+        </Avatar>
+        
+        <Box bgColor={'white'} mr={5} ml={5} mt={8} h={380} borderRadius={'20'} >    
+            <Box ml={5} mr={5} mt={7}>
+            <Text bold fontSize={"lg"} mb={1}>
+              Name
+            </Text>
+            <Text mb={1}>
+              Annisa Nur Salsabilla
+            </Text>
+            <Divider/>
+            <Text bold fontSize={"lg"} mb={8}mt={2}>
+              Address
+            </Text>
+            <Divider/>
+            <Text bold fontSize={"lg"} mb={1}mt={2}>
+              Phone Number
+            </Text>
+            <Text mb={1}>
+              08123345879687
+            </Text>
+            <Divider/>
+            <Text bold fontSize={"lg"} mb={1}mt={2}>
+              Gmail
+            </Text>
+            <Text mb={1}>
+              annisalma2217@gmail.com
+            </Text>
+            <Divider/>
+            <Text bold fontSize={"lg"} mb={1}mt={2}>
+              Date Of Birth
+            </Text>
+            <Text mb={1}>
+              17 July 2000
+            </Text>
+            <Divider/>
+            </Box>
+        </Box>
+
+        <Box alignItems={'center'} mt={5}>
+          <Button w={200} borderRadius={15}>
+            Edit Profile
+          </Button>
+        </Box>
+
+        <Box alignItems={'center'} mt={3} mb={9}>
+          <Button w={200} borderRadius={15}>
+            Log out
+          </Button>
+        </Box>
+
+        </ScrollView>
+      </SafeAreaView>
+    </NativeBaseProvider>
+  );
+};
+
+export default MyProfile;
